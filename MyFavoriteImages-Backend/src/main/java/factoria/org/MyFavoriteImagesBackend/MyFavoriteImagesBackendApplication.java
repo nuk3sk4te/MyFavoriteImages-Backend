ddfb849fc3e931df9d0fd2f721bd561f1bf74987
Cris Mouta <82060703+crismouta@users.noreply.github.com>
2024-02-14 09:39:04 +0100

    MyFavoriteImages backend first commit

---
+++ MyFavoriteImages-Backend/src/main/java/factoria/org/MyFavoriteImagesBackend/MyFavoriteImagesBackendApplication.java
@@ -0,0 +1,13 @@
+package factoria.org.MyFavoriteImagesBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFavoriteImagesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFavoriteImagesBackendApplication.class, args);
+	}
+
+}
---
+++ MyFavoriteImages-Backend/src/test/java/factoria/org/MyFavoriteImagesBackend/MyFavoriteImagesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package factoria.org.MyFavoriteImagesBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFavoriteImagesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
